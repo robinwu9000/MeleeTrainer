ee68df4af7fb159f2ddcee21a89eddae656f5765
Robin Wu <robwu@ucdavis.edu>
2016-05-29 01:22:22 -0700

    Built timer component

--- banner.jsx
+++
@@ -1,3 +0,0 @@
-var Banner = React.createClass({
-  
-})
--- main.jsx
+++ main.jsx
@@ -8,7 +8,49 @@ var App = React.createClass({
   }
 })
 
+
+// var Banner = React.createClass({
+//   render: function() {
+//     return (
+//       <div>Name</div>
+//     );
+//   }
+// });
+
+var Timer = React.createClass({
+  getInitialState: function() {
+    return { seconds: 1800 };
+  },
+
+  tick: function() {
+    this.setState({seconds: this.state.seconds - 1});
+    if(this.state.seconds <= 0) {
+      clearInterval(this.interval);
+    }
+  },
+
+  componentDidMount: function() {
+    this.setState({seconds: this.props.seconds || this.state.seconds});
+    this.interval = setInterval(this.tick, 200);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  render: function() {
+    var output = "";
+    var minutes = Math.floor(this.state.seconds / 60);
+    output += minutes + ":";
+    var seconds = Math.floor(this.state.seconds % 60);
+    output += (seconds < 10 ? '0' : '') + seconds;
+    return (
+      <div>{output}</div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <App />,
+  <Timer />,
   $("#content")[0]
 )
